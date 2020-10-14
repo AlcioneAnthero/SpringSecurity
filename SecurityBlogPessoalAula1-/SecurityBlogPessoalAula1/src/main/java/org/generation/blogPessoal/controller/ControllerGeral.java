cce7443b992c8537124aa91e4f7dbf027647e352
unknown <alcione.silva304@hotmail.com>
2020-10-14 17:24:47 -0300

    SpringSecurity

---
+++ SecurityBlogPessoalAula1-/SecurityBlogPessoalAula1/src/main/java/org/generation/blogPessoal/controller/ControllerGeral.java
@@ -0,0 +1,38 @@
+package org.generation.blogPessoal.controller;
+
+import org.generation.blogPessoal.model.Tema;
+import org.generation.blogPessoal.repository.TemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ControllerGeral {
+	//INJEÇÃO DA INTERFACE DE ACESSO AO BANCO (JPA)
+	@Autowired
+	private TemaRepository repository;
+	
+	// abrir pagina login
+	// REDIRECIONA O localhost:8080/    para localhost:8080/index
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	// GET DE OUTRO JEITO // PODERIA SER GET MAPPING
+	@RequestMapping (value ="/cadastrarTema", method=RequestMethod.GET)
+	public String form() {
+		return "formTema";
+
+	}
+	// UM POST NA TABELA TEMA, REDICIONANDO PARA O ENDPOINT/CADASTRATEMA DE VOLTA
+	@PostMapping (value ="/cadastrarTema")
+	public String form (Tema tema) {
+		
+		repository.save(tema);
+		return "redirect:/cadastrarTema";
+	}
+}
